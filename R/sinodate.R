e8f44c0443701be2d9ef318dcbd0d8c466721ff0
chainsawriot <chainsawtiney@gmail.com>
2015-03-28 01:40:59 +0800

    Settle with S4, validity test and prototype

--- R/sinodate.R
+++ R/sinodate.R
@@ -1,22 +1,30 @@
-sinodate <- setRefClass(
-            Class = "sinodate",
-            fields = list(year = "numeric", month = "numeric", day = "numeric",leap = "logical"),
-            methods = list(
-            show = function() {
-                cat(paste0(year, "-", month, "-", day,"\n"))
-            }
-            ))
-sinodates4 <- setClass("sinodates4", representation(year = 'numeric', month = 'numeric', day = 'numeric', leap = 'logical'))
-validity.sinodates4 <- function(object) {
-    
+
+validity.sinodate <- function(object) {
+    if (length(object@year) != 1 | length(object@year) != 1 | length(object@day) != 1) {
+        return("Year, month and day must be singular")
+    } else {
+        return(TRUE)
+    }
 }
 
-x <- sinodates4(year = 2014, month = 12, day = 1, leap = FALSE)
-x@year
+# TODO: Need to check whether the provided leap is correct
+# TODO: tell the users sinodate will be accurate only from 1901 and 2099
 
-isGeneric('show') # show whether is a generic func
-hasMethod('show')
-args(show) # setMethod need to have the same arg below
+# constructor function
+sinodate <- setClass("sinodate", representation(year = 'numeric', month = 'numeric', day = 'numeric', leap = 'logical'), prototype = list(leap = FALSE), validity = validity.sinodate) 
+
+setMethod('show', signature = 'sinodate', definition = 
+          function(object){
+              cat(paste0(object@year, "-", object@month, ifelse(object@leap, "(Leap)", ""), "-", object@day,"\n"))
+          })
+
+#x <- sinodate(year = 2014, month = 12, day = 1, leap = TRUE) # non-leap by default
+#x@year
+
+
+#isGeneric('show') # show whether is a generic func
+#hasMethod('show')
+#args(show) # setMethod need to have the same arg below
 
 setMethod("show", signature = "sinodates4", definition = function(object) {
     cat(paste0(object@year, "-", object@month, "-", object@day, "\n"))
@@ -24,16 +32,3 @@ setMethod("show", signature = "sinodates4", definition = function(object) {
 
 
 utils::str(sinodate)
-
-
-validity.infant <- function(object) {
-    if(!all(sapply(object@data, is.numeric))) {
-        return("data not numeric")
-    } else return(TRUE)
-}
-setClass("infant",
-         representation(ID = "numeric",
-                        sex = "character",
-                        data = "data.frame"),
-         validity=validity.infant)
-x <- new("infant", data=data.frame(age, male.wt, male.ht), sex="male", ID=1)
